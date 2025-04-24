abb5aaa531fe9550f6fb4f272c3d553fd3b1c7d5
irene12345547 <ireneelsajohn8@gmail.com>
2025-04-24 15:01:12 +0530

    initial commit

---
+++ typecasting.py
@@ -0,0 +1,8 @@
+name = "penguin"
+age = 15
+is_student = True 
+weight = 38.5
+print("Name :", name)
+"print ("Data Type of Name is", type(name) )"
+"print("Age :", age)
+print("Data Type of Age is", type(age) )
