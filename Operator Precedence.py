98b2117c38204a59434e0129a4eb6da96ec408c2
irene12345547 <ireneelsajohn8@gmail.com>
2025-05-06 15:06:52 +0530

    divisible numbers

---
+++ Operator Precedence.py
@@ -0,0 +1,13 @@
+w = 4
+e = 5
+r = 8 
+i = 2
+s = 0
+s = (w+e) * r / i;
+print("Value of (w+e) * r / i is", s)
+name = "ema"
+age = 0
+if  name =="ema" or name == "izzah" and age >= 2 :
+    print("Hello !Welcome.")
+else :
+    print("Good Bye !!")
