59ec24dc02dfb0c02d70f7999cde4c7a6dd772f5
irene12345547 <ireneelsajohn8@gmail.com>
2025-05-07 12:34:19 +0530

    Number Swapping

---
+++ Number Swap.py
@@ -0,0 +1,7 @@
+x = 5
+y = 10
+x = input("Enter value of x: ")
+y = input("Enter value of y: ")
+temp = xx = yy = temp
+print("The value of x after swapping:10")
+print("The value of y after swapping:5 ")
