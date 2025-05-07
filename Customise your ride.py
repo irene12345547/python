6c339d42f0155e6b7c567f2acc60c03dda6481d7
irene12345547 <ireneelsajohn8@gmail.com>
2025-05-07 15:26:03 +0530

    Nested condetional statements

---
+++ Customise your ride.py
@@ -0,0 +1,13 @@
+print("Select your ride: ")
+print("1. Bike")
+print("2. Car")
+choice = int(intput("Enter your choice: "))
+if( choice == 1 ):
+    print("What type of bike? ")
+    print("1.Scooty\n")
+    print("2.Scooty\n")
+choice2=int(input("Enter you choice2: "))
+if choice2==1:
+    print("you have selected scooty")
+else:
+    print("you have selected scooter")
---
+++ Electricity Bill.py
@@ -0,0 +1,12 @@
+units = int(input("Please enter Number of Units you Consumed : "))
+if (units < 50):
+    amount = units * 2.60
+    surcharge = 25
+elif(units <= 100):
+    amount = 130 + ((units -50) * 3.25)
+    surcharge = 35
+elif(units <= 200):
+    amount = 130 + 162.50 + ((units - 100) * 5.26)
+    surcharge = 45
+total = amount + surcharge
+print("\nElectricity Bill = %.2f" %total)
